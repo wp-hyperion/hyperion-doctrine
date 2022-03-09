b02522b59f8ea0de5d1590ae001bd1c1a1142eba
d4rk0s <gregory@d4rk0s.net>
2022-03-09 22:14:06 +0100

    Rajout de paquets manquants et Bugfix sur le host dans la config de la bdd

--- src/Service/DoctrineService.php
+++ src/Service/DoctrineService.php
@@ -45,7 +45,7 @@ class DoctrineService
             'user' =>getenv('DB_USER'),
             'password' => getenv('DB_PASSWORD'),
             'dbname' => getenv('DB_NAME'),
-            'host' => getenv('DB_HOST'),
+            'host' => getenv('DB_HOST') ?: 'localhost',
             'prefix' => getenv('DB_PREFIX') ?: 'wp_'
         ];
     }
